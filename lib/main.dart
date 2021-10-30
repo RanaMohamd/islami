f21c34670c37282136711c305a1ba4830885333c
Rana Mohamed <ranam.shady11@gmail.com>
2021-10-29 22:37:42 -0500

    intial project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:islami/HomeScreen.dart';
+
+void main(){
+
+}
+class MainApplication extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Islami',
+      routes:{
+        HomeScreen.routeName :(buildContext)=>HomeScreen()
+
+      } ,
+      initialRoute: HomeScreen.routeName ,
+    );
+
+  }
+}
